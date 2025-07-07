810fe8b1c05e5c4f6de63eb7946a27ff91072e8c
Yuje Lee <dbwp031@gmail.com>
2025-07-07 23:59:38 +0900

    [10강-1] 비결정적인 테스트 코드 추가

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,119 @@
+package com.example.demo.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.example.demo.model.UserStatus;
+
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:test-application.properties")
+class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	void UserRepository_가_제대로_연결되었다() {
+		//given
+		UserEntity userEntity = new UserEntity();
+		userEntity.setEmail("kok202@naver.com");
+		userEntity.setAddress("Seoul");
+		userEntity.setNickname("kok202");
+		userEntity.setStatus(UserStatus.ACTIVE);
+		userEntity.setCertificationCode("aaaaaa-aaa-aaaaaaa");
+
+		//when
+		UserEntity result = userRepository.save(userEntity);
+
+		//then
+		assertThat(result.getId()).isNotNull();
+	}
+	/*
+	* 전체 테스트로 돌리면 테스트 실패
+	* 혼자 돌리면 테스트 통과
+	* => 비결정적이 테스트
+	* => 테스트 매서드가 병렬적으로 처리되기 때문에 전체 테스트시 실패하게 된다.
+	* */
+	@Test
+	void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+			//given
+			UserEntity userEntity = new UserEntity();
+			userEntity.setEmail("kok202@naver.com");
+			userEntity.setId(1L);
+			userEntity.setAddress("Seoul");
+			userEntity.setNickname("kok202");
+			userEntity.setStatus(UserStatus.ACTIVE);
+			userEntity.setCertificationCode("aaaaaa-aaa-aaaaaaa");
+
+			//when
+  			userRepository.save(userEntity);
+			Optional<UserEntity> result = userRepository.findByIdAndStatus(userEntity.getId(), UserStatus.ACTIVE);
+
+			//then
+			assertThat(result.isPresent()).isTrue();
+	}
+
+	@Test
+	void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다(){
+		//given
+		UserEntity userEntity = new UserEntity();
+		userEntity.setEmail("kok202@naver.com");
+		userEntity.setId(1L);
+		userEntity.setAddress("Seoul");
+		userEntity.setNickname("kok202");
+		userEntity.setStatus(UserStatus.ACTIVE);
+		userEntity.setCertificationCode("aaaaaa-aaa-aaaaaaa");
+
+		//when
+		userRepository.save(userEntity);
+		Optional<UserEntity> result = userRepository.findByIdAndStatus(userEntity.getId(), UserStatus.PENDING);
+
+		//then
+		assertThat(result.isEmpty()).isTrue();
+	}
+
+	@Test
+	void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+		//given
+		UserEntity userEntity = new UserEntity();
+		userEntity.setEmail("kok202@naver.com");
+		userEntity.setId(1L);
+		userEntity.setAddress("Seoul");
+		userEntity.setNickname("kok202");
+		userEntity.setStatus(UserStatus.ACTIVE);
+		userEntity.setCertificationCode("aaaaaa-aaa-aaaaaaa");
+
+		//when
+		userRepository.save(userEntity);
+		Optional<UserEntity> result = userRepository.findByEmailAndStatus(userEntity.getEmail(), UserStatus.ACTIVE);
+
+		//then
+		assertThat(result.isPresent()).isTrue();
+	}
+
+	@Test
+	void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다(){
+		//given
+		UserEntity userEntity = new UserEntity();
+		userEntity.setEmail("kok202@naver.com");
+		userEntity.setId(1L);
+		userEntity.setAddress("Seoul");
+		userEntity.setNickname("kok202");
+		userEntity.setStatus(UserStatus.ACTIVE);
+		userEntity.setCertificationCode("aaaaaa-aaa-aaaaaaa");
+
+		//when
+		userRepository.save(userEntity);
+		Optional<UserEntity> result = userRepository.findByEmailAndStatus(userEntity.getEmail(), UserStatus.PENDING);
+
+		//then
+		assertThat(result.isEmpty()).isTrue();
+	}
+}
