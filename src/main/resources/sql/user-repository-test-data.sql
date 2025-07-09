c99eb9a47b700e63ff7a9b456d90aaa8d4107f10
Yuje Lee <dbwp031@gmail.com>
2025-07-09 22:00:06 +0900

    [11강-2] read 메소드에 테스트 코드 추가

--- src/main/resources/sql/user-repository-test-data.sql
+++ src/main/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,6 @@
 INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values (1, 'kok202@naver.com', 'kok202', 'Seoul', 'aaaaaa-aaa-aaaaaaa', 'ACTIVE', 0);
+
+
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'kok303@naver.com', 'kok303', 'Seoul', 'aaaaaa-aaa-aaaaaaa', 'PENDING', 0);
